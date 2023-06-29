4bfe61f5b92d29e16dd6a1c4b6766bcf166863c8
coder_racer <admin@racer-cr.ru>
2023-06-29 20:13:35 +0600

    fucking news

--- app/Services/KafuSiteServices.php
+++ app/Services/KafuSiteServices.php
@@ -7,16 +7,9 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class KafuSiteServices
 {
-    public function __construct(private HttpRequest $httpRequest)
-    {
-
-    }
-
     public function getNewsAction($page = 1)
     {
-        $this->httpRequest->setUrl(env('KAFU_NEWS') . $page);
-        $response = $this->httpRequest->get();
-        $crawler = new Crawler($response->getContent());
+        $crawler = new Crawler(file_get_contents(env('KAFU_NEWS') . $page));
         return $crawler->filter('.article-content.posts-list a')->each(function ($node) {
             $titleNode = $node->filter('h2');
             $imgNode = $node->filter('img');
